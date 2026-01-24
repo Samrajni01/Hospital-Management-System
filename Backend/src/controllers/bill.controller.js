3ca8e62ad6e80c41c84548e243c3109e04807ff6
Samrajni01 <bhowmiksamrajni@gmail.com>
2026-01-24 23:06:03 +0530

    new

--- Backend/src/controllers/bill.controller.js
+++ Backend/src/controllers/bill.controller.js
@@ -1,4 +1,4 @@
-import { Bill } from "../models/Bill.model.js";
+import { Bill } from "../models/bill.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
